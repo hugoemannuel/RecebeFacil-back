0f54209aa255c5b9d3aad15f19a904f313aa6597
hugoemannuel <hugoemannuel53@gmail.com>
2026-04-27 19:53:53 -0300

    feat: initialize NestJS application with Prisma database service and environment configuration

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,24 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    private readonly logger = new Logger(PrismaService.name);
+
     constructor() {
         const pool = new Pool({ connectionString: process.env.DATABASE_URL });
         const adapter = new PrismaPg(pool);
-        super({ adapter });
+
+        super({
+            adapter,
+            log: ['query', 'info', 'warn', 'error']
+        });
     }
 
     async onModuleInit() {
         await this.$connect();
+        this.logger.log('Banco de dados conectado');
     }
 }
